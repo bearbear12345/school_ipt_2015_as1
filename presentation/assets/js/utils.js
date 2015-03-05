7e8f321639330598a4f8466e1a3f78d99f1ea654
Andrew Wong <andrew12345@live.com>
2015-03-06 01:01:02 +1100

    * Site work

--- presentation/assets/js/utils.js
+++ presentation/assets/js/utils.js
@@ -1,19 +1,3 @@
-function util_enterFullscreen() {
-    var element = document.documentElement;
-    try {
-        element.mozRequestFullScreen();
-        element.webkitRequestFullScreen();
-        element.requestFullscreen();
-        element.msRequestFullscreen();
-    } catch (e) {}
-}
-
-function util_exitFullscreen() {
-    var element = document.documentElement;
-    try {
-        element.mozCancelFullScreen();
-        element.webkitExitFullScreen();
-        element.exitFullscreen();
-        element.msExitFullscreen();
-    } catch (e) {}
+function showPage() {
+	document.getElementById("zWrapper").style.opacity=1;
 }
---
+++ presentation/assets/js/utils_fullscreen.js
@@ -0,0 +1,44 @@
+function util_enterFullscreen() {
+  var element = document.documentElement;
+  try {
+    element.mozRequestFullScreen();
+    element.webkitRequestFullScreen();
+    element.requestFullscreen();
+    element.msRequestFullscreen();
+  } catch (e) {}
+}
+
+function util_exitFullscreen() {
+  var element = document.documentElement;
+  try {
+    element.mozCancelFullScreen();
+    element.webkitExitFullScreen();
+    element.exitFullscreen();
+    element.msExitFullscreen();
+  } catch (e) {}
+}
+
+function FSNotice_dismiss() {
+  document.getElementById("xFS_Notice_bg").style.display = "none";
+  document.getElementById("xFS_Notice").style.display = "none";
+  showPage();
+}
+
+function FSNotice_accept() {
+  util_enterFullscreen();
+  FSNotice_dismiss();
+}
+
+function FSNotice_deny() {
+  sessionStorage.setItem("zFullscreenPrompt", "never");
+  FSNotice_dismiss();
+}
+
+function FSNotice_checkFullscreen_and_Load() {
+  if (!window.fullScreen && (!sessionStorage.hasOwnProperty("zFullscreenPrompt") || sessionStorage.getItem("zFullscreenPrompt") != "never")) {
+    document.getElementById("xFS_Notice_bg").style.display = "inline";
+    document.getElementById("xFS_Notice").style.display = "inline";
+  } else {
+    showPage();
+  }
+}
