86e788a347295399ca7da275e1405c75202f1e30
Andrew Wong <andrew12345@live.com>
2015-03-08 18:48:34 +1100

    + Add transition

--- presentation/assets/js/presentation.js
+++ presentation/assets/js/presentation.js
@@ -5,6 +5,8 @@ function presentation_showPage() {
 function presentation_goMainMenu() {
   document.getElementById("xCategory").style.display = "none";
   document.getElementById("xTiles").style.display = "inline";
+  document.getElementById("zHomeIcon").style.opacity = 0.1;
+  presentation_themeColour("#BFBFBF");
 }
 
 function presentation_selectCategory(elem) {
@@ -15,22 +17,11 @@ function presentation_selectCategory(elem) {
   document.getElementById("xCategoryThree").style.display = "none";
   document.getElementById("xCategoryFour").style.display = "none";
   document.getElementById(elem.id.slice(0, -4)).style.display = "inline";
-  /*  switch (cat) {
-      case 1:
-        document.getElementById("xCategoryOne").style.display = "inline";
-        break;
-      case 2:
-        document.getElementById("xCategoryTwo").style.display = "inline";
-        break;
-      case 3:
-        document.getElementById("xCategoryThree").style.display = "inline";
-        break;
-      case 4:
-        document.getElementById("xCategoryFour").style.display = "inline";
-        break;
-      default:
-        break;
+  presentation_themeColour("rgba"+window.getComputedStyle(document.getElementById(elem.id)).backgroundColor.slice(3,-1)+", 0.5)");
+  document.getElementById("zHomeIcon").style.opacity = 1;
+}
 
-    }
-    */
+function presentation_themeColour(colour) {
+	document.getElementById("zHeader").style.backgroundColor = colour;
+	document.getElementById("zFooter").style.backgroundColor = colour;
 }
