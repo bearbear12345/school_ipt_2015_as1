00b59585e8300995796822f84d53b8b90daeb67a
Andrew Wong <andrew12345@live.com>
2015-03-08 23:32:31 +1100

    * Minor changes for mobile devices

--- presentation/assets/js/loader.js
+++ presentation/assets/js/loader.js
@@ -1,11 +1,11 @@
 window.onload = function() {
-  FSNotice_checkFullscreen_and_Load();
   if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     document.getElementById("zContextMenuTip").innerHTML = "";
 	for (i=0;i<document.getElementsByClassName("xCategory_tip").length;i++){
 	  document.getElementsByClassName("xCategory_tip")[i].style.opacity=1;
 	}
   } else {
+	FSNotice_checkFullscreen_and_Load();
     zContextMenu_createElement();
   }
 }
